4a70769231275ef1d6469eeeea01f01f33e5f366
natsteve124 <107065263+natsteve124@users.noreply.github.com>
2022-12-09 19:14:36 -0800

    update jobhunter.py
    - finished adding new job function
    - finished checking if job exist function
    - finished notifying user if job added

--- jobhunter.py
+++ jobhunter.py
@@ -5,23 +5,21 @@ import requests
 from datetime import date
 import html2text
 
-
 # Connect to database
-# You may need to edit the connect function based on your local settings.#I made a password for my database because it is important to do so. Also make sure MySQL server is running or it will not connect
+# You may need to edit the connect function based on your local settings.#I made a password for my database because it is important to do so. Also make sure MySQL server is running, or it will not connect
 def connect_to_sql():
-    conn = mysql.connector.connect(user='root', password='',
-                                   host='127.0.0.1', database='cne340')
+    conn = mysql.connector.connect(
+        user='root', password='',
+        host='127.0.0.1', database='cne340'
+    )
     return conn
-
 
 # Create the table structure
 def create_tables(cursor):
     # Creates table
     # Must set Title to CHARSET utf8 unicode Source: http://mysql.rjweb.org/doc.php/charcoll.
     # Python is in latin-1 and error (Incorrect string value: '\xE2\x80\xAFAbi...') will occur if Description is not in unicode format due to the json data
-    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs (id INT PRIMARY KEY auto_increment, Job_id varchar(50) , 
-    company varchar (300), Created_at DATE, url varchar(30000), Title LONGBLOB, Description LONGBLOB ); ''')
-
+    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs (id INT PRIMARY KEY auto_increment, Job_id varchar(50), company varchar(300), Created_at DATE, url varchar(30000), Title LONGBLOB, Description LONGBLOB);''')
 
 # Query the database.
 # You should not need to edit anything in this function
@@ -29,23 +27,34 @@ def query_sql(cursor, query):
     cursor.execute(query)
     return cursor
 
-
 # Add a new job
 def add_new_job(cursor, jobdetails):
     # extract all required columns
-    description = html2text.html2text(jobdetails['description'])
+    job_id = jobdetails["id"]
+    company = jobdetails["company_name"]
     date = jobdetails['publication_date'][0:10]
-    query = cursor.execute("INSERT INTO jobs( Description, Created_at " ") "
-               "VALUES(%s,%s)", (  description, date))
+    url = jobdetails["url"]
+    title = jobdetails["title"]
+    description = html2text.html2text(jobdetails['description'])
+
+    query = cursor.execute(
+        "INSERT INTO jobs(Job_id, company, Created_at, url, Title, Description)"
+        "VALUES(%s, %s, %s, %s, %s, %s)", (job_id, company, date, url, title, description)
+    )
      # %s is what is needed for Mysqlconnector as SQLite3 uses ? the Mysqlconnector uses %s
     return query_sql(cursor, query)
-
 
 # Check if new job
 def check_if_job_exists(cursor, jobdetails):
     ##Add your code here
-    query = "UPDATE"
-    return query_sql(cursor, query)
+    query = "SELECT Job_id FROM jobs WHERE Job_id = " + str(jobdetails["id"])
+    cursor = query_sql(cursor, query)
+    # fetchone returns None if row doesn't exist -> return False
+    if cursor.fetchone() is None:
+        return False
+    # otherwise -> return True
+    else:
+        return True
 
 # Deletes job
 def delete_job(cursor, jobdetails):
@@ -53,14 +62,11 @@ def delete_job(cursor, jobdetails):
     query = "UPDATE"
     return query_sql(cursor, query)
 
-
 # Grab new jobs from a website, Parses JSON code and inserts the data into a list of dictionaries do not need to edit
 def fetch_new_jobs():
     query = requests.get("https://remotive.io/api/remote-jobs")
     datas = json.loads(query.text)
-
     return datas
-
 
 # Main area of the code. Should not need to edit
 def jobhunt(cursor):
@@ -70,21 +76,17 @@ def jobhunt(cursor):
     # print(jobpage)
     add_or_delete_job(jobpage, cursor)
 
-
 def add_or_delete_job(jobpage, cursor):
     # Add your code here to parse the job page
     for jobdetails in jobpage['jobs']:  # EXTRACTS EACH JOB FROM THE JOB LIST. It errored out until I specified jobs. This is because it needs to look at the jobs dictionary from the API. https://careerkarma.com/blog/python-typeerror-int-object-is-not-iterable/
         # Add in your code here to check if the job already exists in the DB
-        check_if_job_exists(cursor, jobdetails)
-        is_job_found = len(
-        cursor.fetchall()) > 0  # https://stackoverflow.com/questions/2511679/python-number-of-rows-affected-by-cursor-executeselect
-        if is_job_found:
-
-        else:
+        # not True -> False
+        # not False -> True
+        if not check_if_job_exists(cursor, jobdetails):
             # INSERT JOB
+            add_new_job(cursor, jobdetails)
             # Add in your code here to notify the user of a new posting. This code will notify the new user
-
-
+            print("Job added")
 
 # Setup portion of the program. Take arguments and set up the script
 # You should not need to edit anything here.
@@ -94,10 +96,11 @@ def main():
     conn = connect_to_sql()
     cursor = conn.cursor()
     create_tables(cursor)
+    cursor.execute("ALTER TABLE jobs CHANGE company company VARCHAR(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
 
     while (1):  # Infinite Loops. Only way to kill it is to crash or manually crash it. We did this as a background process/passive scraper
         jobhunt(cursor)
-        time.sleep(21600)  # Sleep for 1h, this is ran every hour because API or web interfaces have request limits. Your reqest will get blocked.
+        time.sleep(21600)  # Sleep for 1h, this is run every hour because API or web interfaces have request limits. Your request will get blocked.
 
 
 # Sleep does a rough cycle count, system is not entirely accurate
